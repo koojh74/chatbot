0d7b9ad5d5ff2556dc16bc91c99c06347663285c
Jahyoung Koo <koojh74@gmail.com>
2024-12-06 16:02:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,9 +79,9 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Display the existing chat messages via `st.chat_message`.
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
